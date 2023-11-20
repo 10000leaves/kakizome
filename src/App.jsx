4508793961a57c59811625be3cd9d4ef5db56ff8
10000leaves <53398740+10000leaves@users.noreply.github.com>
2023-11-20 04:50:16 +0000

    feat: ファーストコミット

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Canvas from './Canvas';
+
+function App() {
+  return (
+    <div>
+      <h1>Happy New Year!</h1>
+      <Canvas width={800} height={600} />
+    </div>
+  );
+};
+  
+export default App;
